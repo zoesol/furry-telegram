15a38c626e60dbcb6702a1001aefecc09093f896
Zoe Solis <zoe.solis25@gmail.com>
2019-09-03 19:15:42 -0700

    repositioned buttons and changed text color on home page

--- furryTelegram/HomePage.js
+++ furryTelegram/HomePage.js
@@ -1,27 +1,33 @@
 import React, { Component } from 'react';
 import { StyleSheet, Button, Text, View } from 'react-native';
-import DialogInput from 'react-native-dialog-input';
+import Dialog from 'react-native-dialog';
 import Habit from './Habit';
 import LongTermGoal from './LongTermGoal';
+import { YellowBox } from 'react-native';
 
+YellowBox.ignoreWarnings([
+  'Warning: componentWillReceiveProps is deprecated',
+]);
 
 const styles = StyleSheet.create({
   homePage: {
+    flex: 1,
     position: "absolute",
     alignItems: "center",
     width: '100%',
     height: '100%',
   },
   habitGroup: {
-    flex: 1,
     width: '100%',
-    height: '100%',
+    height: '50%',
+    top: '95%',
     justifyContent: 'space-between',
     alignItems: 'center',
     alignContent: 'center',
     flexWrap: 'wrap',
     flexDirection: 'row',
     position: 'relative',
+    backgroundColor: 'lightgrey',
   },
   habit: {
     width: 80, height: 80, margin: 10,
@@ -36,14 +42,22 @@ const styles = StyleSheet.create({
     top: '10%',
   },
   addHabitButton: {
-    right: '35%',
-    backgroundColor:"purple"
+    position: 'absolute',
+    top: '3%',
+    left: '10%',
+    backgroundColor:"purple",
+    width: '30%'
   },
   goalsButton: {
-    left: '35%',
-    backgroundColor:"purple"
+    top: '3%',
+    position: 'absolute',
+    right: '10%',
+    backgroundColor:"purple",
+    width: '30%'
+  },
+  habitButtonText: {
+    textAlign: 'center'
   }
-
 });
 
 export default class HomePage extends Component {
@@ -84,7 +98,7 @@ export default class HomePage extends Component {
   logHabit = (inputText) => {
     this.togglePressStatus(this.state.lastPressed)
     this.state.habits[this.state.lastPressed].setHistory(inputText)
-    this.setState({isDialogVisible: false})
+    this.setState({isDialogVisible: false, noteText: ""})
   }
   togglePressStatus = (i) => {
     this.setState((previousState) => {
@@ -108,13 +122,13 @@ export default class HomePage extends Component {
   }
   addDiaglogInputComponent = () => {
     return (
-      <DialogInput isDialogVisible={this.state.isDialogVisible}
-        title={'DialogInput'}
-        message={"Message for DialogInput"}
-        hintInput ={"HINT INPUT"}
-        submitInput={ (inputText) => {this.logHabit(inputText)} }
-        closeDialog={ () => {this.handleCloseDialog()}}>
-      </DialogInput>
+      <Dialog.Container visible={this.state.isDialogVisible} contentStyle={{height: 300, paddingBottom: 120}}>
+        <Dialog.Title>Dialog Input</Dialog.Title>
+        <Dialog.Description>Message for Dialog Input</Dialog.Description>
+        <Dialog.Input height="100%" multiline={true} onChangeText={(inputtxt)=> this.setState({noteText: inputtxt})}></Dialog.Input>
+        <Dialog.Button label="Cancel" onPress={this.handleCloseDialog}></Dialog.Button>
+        <Dialog.Button label="Submit" onPress={() => this.logHabit(this.state.noteText)}></Dialog.Button>
+      </Dialog.Container> 
     )
   }
   addHabitButtonComponents = () => {
@@ -126,6 +140,7 @@ export default class HomePage extends Component {
               <Button
                 title={habit.habit_name}
                 onPress={() => this.handleHabitButtonClick(i)}
+                color="black"
               />
               <Text>
                 {habit.getProgress()}
@@ -156,6 +171,7 @@ export default class HomePage extends Component {
           onPress={() => this.props.navigation.navigate('AddHabit', {
             addHabitCallback: this.addNewHabit
           })}
+          color='white'
         />
       </View>
     )
@@ -170,6 +186,7 @@ export default class HomePage extends Component {
             goals: this.state.goals,
             addGoalCallback: this.addNewGoal
           })}
+          color='white'
         />
       </View>
     )
@@ -178,7 +195,7 @@ export default class HomePage extends Component {
     return (
       <View style={styles.pageTitle}>
         <Text>
-          Home Pages
+          Home
         </Text>
       </View>
     )
